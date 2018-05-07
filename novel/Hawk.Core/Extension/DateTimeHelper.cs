8962341767236cb292d470b9f14a8423fda249df
hawk <tianlt2012@foxmail.com>
2018-05-08 00:24:21 +0800

    update date-help

--- novel/Hawk.Core/Extension/DateTimeHelper.cs
+++ novel/Hawk.Core/Extension/DateTimeHelper.cs
@@ -9,38 +9,46 @@ namespace Hawk
         /// </summary>
         /// <returns></returns>
         public static string ToFullString(this DateTime dateTime)
-        {
-            return dateTime.ToString(Field.DATE_FULL_STRING);
-        }
+        => dateTime.ToString(Field.DATE_FULL_STRING);
 
         /// <summary>
         /// 格式化为{yyyy-MM-dd HH:mm:ss}
         /// </summary>
         /// <returns></returns>
         public static string ToDateString(this DateTime dateTime)
-        {
-            return dateTime.ToString(Field.DATE_STRING);
-        }
+        => dateTime.ToString(Field.DATE_STRING);
+
 
         /// <summary>
         /// 格式化为{yyyy-MM-dd}
         /// </summary>
         /// <returns></returns>
         public static string ToShortString(this DateTime dateTime)
-        {
-            return dateTime.ToString(Field.DATE_SHORT_STRING);
-        }
+        => dateTime.ToString(Field.DATE_SHORT_STRING);
 
-        public static long ToUnixSeconds(this DateTime date)
-            => (date.ToUniversalTime().Ticks - 621355968000000000L) / 10000000L;
+        /// <summary>
+        ///  返回unix时间戳
+        /// </summary>
+        /// <param name="milli">毫秒</param>
+        /// <returns></returns>
+        public static long ToTimestamp(this DateTime date, bool milli = false)
+            => (date.ToUniversalTime().Ticks - 621355968000000000L) / (milli ? 10000L : 10000000L);
 
-        public static long ToUnixSeconds(this DateTimeOffset date)
+        /// <summary>
+        ///  返回unix时间戳
+        /// </summary>
+        /// <param name="milli">毫秒</param>
+        /// <returns></returns>
+        public static long ToTimestamp(this DateTimeOffset date, bool milli = false)
         {
 #if NET40 || NET45
-            return (date.ToUniversalTime().Ticks - 621355968000000000L) / 10000000L;
+            return (date.ToUniversalTime().Ticks - 621355968000000000L) / (milli ? 10000L : 10000000L);
 #else
-            return date.ToUnixTimeSeconds();
+            return milli ? date.ToUnixTimeMilliseconds() : date.ToUnixTimeSeconds();
 #endif
         }
+
+        public static DateTime ToDate(this int timestamp)
+            => (Field.EPOCH + TimeSpan.FromSeconds(timestamp)).ToLocalTime();
     }
 }
